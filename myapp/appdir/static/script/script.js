00d5d7403240212388eca47df340f09841fd0d0d
Arno Li <guiwecgdiu@163.com>
2019-12-11 22:41:39 +0800

    improve

---
+++ myapp/appdir/static/script/script.js
@@ -0,0 +1,17 @@
+
+function a() {
+    alert("It work")
+}
+
+
+function post_valids() {
+    let post=document.querySelector("#postSubmit");
+    post.color="green";
+}
+  function close_flash_message(){
+            let ul = document.querySelector('ul');
+            ul.innerHTML="";
+        }
+   function post() {
+       alert("This is a post")
+   }
